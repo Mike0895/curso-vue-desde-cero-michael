58be6850749c006098497e81317135c6ced2a2e2
Mike0895 <michael.morocho05@gmail.com>
2024-05-05 20:34:49 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const API = "https://api.github.com/users/";
+
+
+const app = Vue.createApp({
+    data() {
+        return {
+            message: "Hello Vue! fire",
+        }
+    },
+    methods: {
+    async doSearch() {
+    const response = await fetch(API + 'juanwmedia')
+    const data = await response.json()
+    console.log(data)
+
+}
+
+    }
+})
+
